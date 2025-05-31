976e8ff7a8bfa3280b7e947ace676f8edf82a088
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-05-31 12:47:46 +0400

    Update 01_if_else.cpp

--- cpp-programming/control flow/01_if_else.cpp
+++ cpp-programming/control flow/01_if_else.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num > 0)
+        cout << "It's a Positive number\n";
+    else if (num < 0)
+        cout << "It's a Negative number\n";
+    else
+        cout << "Zero\n";
+
+    return 0;
+}
 
