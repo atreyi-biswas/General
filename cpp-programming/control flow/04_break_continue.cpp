f0d72636cdf71558d193855a0d8b16295f527450
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-05-31 12:53:17 +0400

    Update 04_break_continue.cpp

--- cpp-programming/control flow/04_break_continue.cpp
+++ cpp-programming/control flow/04_break_continue.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        if (i == 5)
+            continue;  //will skip 5
+        if (i == 8)
+            break;     //will stop at 8
+        cout << i << " ";
+    }
+
+    return 0;
+}
 
