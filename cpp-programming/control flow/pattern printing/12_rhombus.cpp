78673a0239a4756788d3f4b9d4b2acd211edffe5
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-06-04 10:23:01 +0400

    Create 12_rhombus.cpp

---
+++ cpp-programming/control flow/pattern printing/12_rhombus.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "No. of rows and columns: ";
+    cin>>n;
+    int a=2*n-1;
+
+    for(int i=1 ; i<=n ; i++){
+        
+        for(int j=1 ; j<=a-i ; j++){
+            cout<<" ";
+            
+
+        }
+        for(int k=1 ; k<=n ; k++){
+            cout<<"*";
+        }
+        cout<< "\n";
+
+    }
+  
+}
