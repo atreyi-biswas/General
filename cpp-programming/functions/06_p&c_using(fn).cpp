f07a5292504f8ad1fc2310e6ebd72d003c1f9202
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-06-14 11:18:23 +0400

    Update and rename 06_recursion.cpp to 06_p&c_using(fn).cpp

---
+++ cpp-programming/functions/06_p&c_using(fn).cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+
+int fact(int x){
+    int f = 1;
+    for(int i=1; i<=x; i++){
+    f=f*i;
+   }
+    return f;
+}
+
+int combination(int n, int r){
+    int com = fact(n) / (fact(r)*fact(n-r));
+
+    return com;
+}
+
+int permutation(int n, int r){
+    int per = fact(n) / fact(n-r);
+
+    return per;
+}
+int main() {
+    int n,r;
+    cout<<"Enter value of n: ";
+    cin>>n;
+    cout<<"Enter value of r: ";
+    cin>>r;
+    
+    cout<<permutation(n,r)<<endl;
+    cout<<combination(n,r)<<endl; 
+}
