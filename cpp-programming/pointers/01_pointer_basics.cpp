858dc353e1389c9300dbb28246e6b1fb63189e7e
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-06-21 10:58:25 +0400

    Update 01_pointer_basics.cpp

--- cpp-programming/pointers/01_pointer_basics.cpp
+++ cpp-programming/pointers/01_pointer_basics.cpp
@@ -1,1 +1,11 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int x = 4;
+    int *ptr = &x;
+
+    cout<<&x<<endl;
+    cout<<ptr;   //prints address
+    cout<<*ptr;   //prints value
+}
