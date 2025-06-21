3580232bb42f5f0aad063049c726bb17f48a3e56
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-06-21 10:52:28 +0400

    Update 04_vector_basics.cpp

--- cpp-programming/arrays/04_vector_basics.cpp
+++ cpp-programming/arrays/04_vector_basics.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+#include<vector>
 
+int main(){
+    vector<int> v;
+    v.push_back(1);    //creating elements in array
+    v.push_back(5);
+    v.push_back(10);
+    v.push_back(0);
+    v.push_back(6);
+    v.push_back(3);
+    v.push_back(0);
+    v.push_back(9);
+    v.push_back(2);
+    v.push_back(7);
+    v.push_back(4);
+    v.push_back(8);
+
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+  
+    v.pop_back()   //removing elements from array from the end
+}
