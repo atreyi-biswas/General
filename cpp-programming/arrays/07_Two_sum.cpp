11ab24bdedab33badff75bfd5ba88ffa7033fabe
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-06-23 12:04:54 +0400

    Create 07_Two_sum.cpp

---
+++ cpp-programming/arrays/07_Two_sum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+
+int main(){
+    int x;
+    cout<<"Enter target: ";
+    cin>>x;
+    vector<int> v;
+    int n;
+    cout<<"Enter array size: ";
+    cin>>n;
+
+    cout<<"Enter array elements: ";
+    for(int i=0; i<=n; i++){
+        int q;
+        cin>>q;
+        v.push_back(q);
+    }
+    
+    for(int i=0; i<=v.size()-2; i++){        //to avoid repetitions like (0,a) and (a,0)
+        for(int j=i; j<=v.size()-1; j++){
+            if(v[i]+v[j]==x){
+                cout<<"("<<i<<","<<j<<")";
+            }
+        }
+    }
+ 
+}
