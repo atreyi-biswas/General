a583c9b80c8c58007aa082221a9a7ad96c058210
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-06-26 16:52:13 +0400

    Update 05_Transpose_of_Matrix.cpp

--- cpp-programming/arrays/2D arrays/05_Transpose_of_Matrix.cpp
+++ cpp-programming/arrays/2D arrays/05_Transpose_of_Matrix.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter number of rows: ";
+    cin>>m;
+    int n;
+    cout<<"Enter number of columns: ";
+    cin>>n;
 
+    
+    int arr[m][n];
+    cout<<"Enter elements: ";
+    for(int i=0; i<=m-1; i++){
+        for(int j=0; j<=n-1; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+
+    for(int i=0; i<=m-1; i++){
+        for(int j=0; j<=n-1; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    for(int j=0; j<=n-1; j++){
+        for(int i=0; i<=m-1; i++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
