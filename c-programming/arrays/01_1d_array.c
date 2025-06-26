5e9df240b47daeba261e2f0297b47153103badf2
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-06-26 13:06:51 +0400

    Update 01_1d_array.c

--- c-programming/arrays/01_1d_array.c
+++ c-programming/arrays/01_1d_array.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int sum = 0;
+    float avg;
+
+    printf("Array elements: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+        sum += arr[i];
+    }
+
+    avg = (float)sum / 5;
+
+    printf("\nSum = %d\n", sum);
+    printf("Average = %.2f\n", avg);
+
+    return 0;
+}
 
