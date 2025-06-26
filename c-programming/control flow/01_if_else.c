c59385389070089e7cd478f9f8f15dde7d96c1c6
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-06-26 12:53:13 +0400

    Update 01_if_else.c

--- c-programming/control flow/01_if_else.c
+++ c-programming/control flow/01_if_else.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 0)
+        printf("It is a Positive number\n");
+    else if (num < 0)
+        printf("It is a Negative number\n");
+    else
+        printf("The number is Zero\n");
+
+    return 0;
+}
 
