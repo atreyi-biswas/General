14219cc3636f0fc5f672cd0f59aee592001caac3
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-06-26 12:57:19 +0400

    Update 02_switch_case.c

--- c-programming/control flow/02_switch_case.c
+++ c-programming/control flow/02_switch_case.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, count = 0;
+
+    printf("Enter a word: ");
+    scanf("%s", str);
+
+    while (str[i] != '\0') {
+        switch (str[i]) {
+            case 'a': case 'e': case 'i': case 'o': case 'u':
+            case 'A': case 'E': case 'I': case 'O': case 'U':
+                count++;
+                break;
+        }
+        i++;
+    }
+
+    printf("Number of vowels: %d\n", count);
+
+    return 0;
+}
 
