c9514d28286bc7af29c0c33f91eaeb967c740120
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-06-26 13:09:12 +0400

    Update 05_array_functions.c

--- c-programming/arrays/05_array_functions.c
+++ c-programming/arrays/05_array_functions.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+void printArray(int a[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+}
+
+int sumArray(int a[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+        sum += a[i];
+    return sum;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = 5;
+
+    printf("Array elements: ");
+    printArray(arr, size);
+
+    int sum = sumArray(arr, size);
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
 
