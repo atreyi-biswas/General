3473062ab29f7dab8d6665ba965d71def8e713cc
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-06-26 13:00:08 +0400

    Update 06_break_continue.c

--- c-programming/control flow/06_break_continue.c
+++ c-programming/control flow/06_break_continue.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        if (i == 3)
+            continue;  // Skip iteration when i is 3
+        if (i == 6)
+            break;     // Exit loop when i is 6
+        printf("%d ", i);
+    }
+    return 0;
+}
 
