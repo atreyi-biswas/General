c54d7f79daf4ef4ae01dc0653fb88541c873e16d
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-08-03 11:56:53 +0530

    Update 02_function_with_arguments.c

--- c-programming/functions/02_function_with_arguments.c
+++ c-programming/functions/02_function_with_arguments.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+// Function definition with arguments
+void add(int x, int y) {
+    int sum = x + y;
+    printf("Sum of %d and %d is %d\n", x, y, sum);
+}
+
+int main() {
+    add(5, 7);  // call with arguments
+    add(10, 20); // another call with different arguments
+    return 0;
+}
 
