f5d330c4baaa15e5882e9055bdcc4a856cc0ff55
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-08-03 11:59:16 +0530

    Update 04_recursion.c

--- c-programming/functions/04_recursion.c
+++ c-programming/functions/04_recursion.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+// Recursive function to calculate factorial
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;  // Base case
+    else
+        return n * factorial(n - 1);  // Recursive call
+}
+
+int main() {
+    int num = 5;
+    int result = factorial(num);
+    
+    printf("Factorial of %d is %d\n", num, result);
+
+    return 0;
+}
 
