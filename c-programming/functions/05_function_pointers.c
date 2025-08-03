4a0c7b37e846d860f1523f0ecea1c914351ad75c
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-08-03 12:00:47 +0530

    Update 05_function_pointers.c

--- c-programming/functions/05_function_pointers.c
+++ c-programming/functions/05_function_pointers.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+// A simple function
+void sayHello() {
+    printf("Hello!\n");
+}
+
+int main() {
+    // Declare a function pointer
+    void (*ptr)() = sayHello;
+
+    // Call the function using the pointer
+    ptr();
+
+    return 0;
+}
+
 
