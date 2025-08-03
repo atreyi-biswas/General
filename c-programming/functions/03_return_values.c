0d1776a3386501e0123058f5a1a145b955e03240
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-08-03 11:58:22 +0530

    Update 03_return_values.c

--- c-programming/functions/03_return_values.c
+++ c-programming/functions/03_return_values.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+// Function that returns an integer value
+int add(int a, int b) {
+    return a + b;  // returns the sum
+}
+
+int main() {
+    int x = 10, y = 15;
+    int result = add(x, y);  // store returned value in "add(a,b)" function
+
+    printf("Result: %d\n", result);
+
+    return 0;
+}
 
