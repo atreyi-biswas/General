12a63cde0d9412d5ba87caef775ccaaf0b136f31
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-08-05 04:22:06 +0530

    Update 02_variables.py

--- python-programming/basics/02_variables.py
+++ python-programming/basics/02_variables.py
@@ -1,1 +1,12 @@
 
+name = "Computer"     # string
+age = 20              # integer
+height = 5.4          # float
+is_student = True     # boolean
+
+#print
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
