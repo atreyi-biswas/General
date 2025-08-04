c6c299d7f4f17ce3616341861accb114e79c1189
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-08-05 04:26:03 +0530

    Update 04_operators.py

--- python-programming/basics/04_operators.py
+++ python-programming/basics/04_operators.py
@@ -1,1 +1,24 @@
+a = 10
+b = 3
+
+# Arithmetic operators
+print("Addition:", a + b)                 #addition
+print("Subtraction:", a - b)              #subtraction
+print("Multiplication:", a * b)           #multiplication
+print("Division:", a / b)                 #divide
+print("Modulus:", a % b)                  #modulus
+print("Exponent:", a ** b)                #exponent
+print("Floor Division:", a // b)          #floor division
+
+# Comparison operators
+print("a > b:", a > b)                    #greater than
+print("a == b:", a == b)                  #equals
+print("a != b:", a != b)                  #not equal to
+
+# Logical operators
+x = True
+y = False
+print("x and y:", x and y)
+print("x or y:", x or y)
+print("not x:", not x)
 
