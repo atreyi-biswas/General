46745ff0522299e4b4159540407f909c2edcfc08
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-08-07 16:45:11 +0530

    Update 03_match_case.py

--- python-programming/control flow/03_match_case.py
+++ python-programming/control flow/03_match_case.py
@@ -1,1 +1,12 @@
+day = input("Enter day: ").lower()
+
+match day:
+    case "monday":
+        print("It's the Start of the week!")
+    case "friday":
+        print("It's the Last working day!")
+    case "saturday" | "sunday":
+        print("It's the Weekend!")
+    case _:
+        print("It's the Mid-week day!")
 
