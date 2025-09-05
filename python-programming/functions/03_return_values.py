598f544bf5fdca8c318a7bb1b15eb7db944066eb
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-09-05 23:55:19 +0530

    Update 03_return_values.py

--- python-programming/functions/03_return_values.py
+++ python-programming/functions/03_return_values.py
@@ -1,1 +1,14 @@
+# Function returning a single value
+def square(x):
+    return x * x
+
+# Function returning multiple values
+def calculate(a, b):
+    return a+b, a-b, a*b, a/b
+
+# Testing
+print("Square of 5:", square(5))
+
+sum, diff, prod, div = calculate(10, 2)
+print("Results:", sum, diff, prod, div)
 
