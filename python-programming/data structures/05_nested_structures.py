eb546e3c62e9ab156785e9773dd8d2e1c295f982
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-09-06 00:02:37 +0530

    Update 05_nested_structures.py

--- python-programming/data structures/05_nested_structures.py
+++ python-programming/data structures/05_nested_structures.py
@@ -1,1 +1,19 @@
+# List of dictionaries
+students = [
+    {"name": "Atreyi", "age": 18},
+    {"name": "Sriti", "age": 20}
+]
+
+print("Students list:", students)
+
+# Dictionary with list values
+courses = {
+    "CSE": ["Python", "C", "DSA"],
+    "ECE": ["Circuits", "Signals"]
+}
+
+print("CSE courses:", courses["CSE"])
+
+# Nested access
+print("First CSE course:", courses["CSE"][0])
 
