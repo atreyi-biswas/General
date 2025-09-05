e83638b0c24420c2111f59a95fbb17f339dad656
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-09-06 00:03:36 +0530

    Update 06_comprehensions.py

--- python-programming/data structures/06_comprehensions.py
+++ python-programming/data structures/06_comprehensions.py
@@ -1,1 +1,12 @@
+# List comprehension
+squares = [x*x for x in range(6)]
+print("Squares:", squares)
+
+# Set comprehension
+evens = {x for x in range(10) if x % 2 == 0}
+print("Evens:", evens)
+
+# Dict comprehension
+square_dict = {x: x*x for x in range(5)}
+print("Square dictionary:", square_dict)
 
