5fdbe717f3f203edb020487c023e5ed89628e33a
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-09-05 23:58:37 +0530

    Update 01_lists.py

--- python-programming/data structures/01_lists.py
+++ python-programming/data structures/01_lists.py
@@ -1,1 +1,19 @@
+# Creating a list
+fruits = ["apple", "banana", "cherry"]
+
+# Append
+fruits.append("orange")
+print("After append:", fruits)
+
+# Pop (removes last element by default)
+fruits.pop()
+print("After pop:", fruits)
+
+# Pop at index
+fruits.pop(1)
+print("After popping index 1:", fruits)
+
+# Slicing
+print("Slicing [0:2]:", fruits[0:2])
+print("Reversed list:", fruits[::-1])
 
