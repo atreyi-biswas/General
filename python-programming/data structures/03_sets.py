6a77d8da0602e22e9631bf70e8133da598dfeaea
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-09-05 23:59:06 +0530

    Update 03_sets.py

--- python-programming/data structures/03_sets.py
+++ python-programming/data structures/03_sets.py
@@ -1,1 +1,17 @@
+# Creating a set
+numbers = {1, 2, 3, 2, 1}
+print("Unique set:", numbers)  # duplicates removed
+
+# Adding element
+numbers.add(4)
+print("After add:", numbers)
+
+# Set operations
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print("Union:", a | b)
+print("Intersection:", a & b)
+print("Difference (a - b):", a - b)
+print("Symmetric difference:", a ^ b)
 
