82ea7ad563f4f9293f4cd68fc77d1077fa6bb354
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-09-05 23:58:53 +0530

    Update 02_tuples.py

--- python-programming/data structures/02_tuples.py
+++ python-programming/data structures/02_tuples.py
@@ -1,1 +1,14 @@
+# Creating a tuple
+coordinates = (10, 20)
+
+# Accessing elements
+print("X:", coordinates[0])
+print("Y:", coordinates[1])
+
+# Tuples are immutable → this will cause an error if uncommented
+# coordinates[0] = 50
+
+# Tuple unpacking
+x, y = coordinates
+print("Unpacked:", x, y)
 
