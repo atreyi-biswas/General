bffe17053fdfb62b91743281763f0dc1a3228a2e
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-09-07 22:39:45 +0530

    Update 01_string_basics.py

--- python-programming/strings/01_string_basics.py
+++ python-programming/strings/01_string_basics.py
@@ -1,1 +1,25 @@
+# Defining strings
+s1 = "Hello"
+s2 = 'World'
+s3 = """This is
+a multiline string"""
+
+print("s1:", s1)
+print("s2:", s2)
+print("s3:", s3)
+
+# Indexing
+print("First character of s1:", s1[0])
+print("Last character of s2:", s2[-1])
+
+# Slicing
+print("First three chars of s1:", s1[:3])
+print("s2 reversed:", s2[::-1])
+
+# Concatenation
+greeting = s1 + " " + s2
+print("Concatenation:", greeting)
+
+# Repetition
+print("Repeat s1 3 times:", s1 * 3)
 
