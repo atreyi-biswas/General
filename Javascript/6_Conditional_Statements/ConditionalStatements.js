37af513f1ac7907a15b23b6776829de04b76f2f7
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-10-31 20:19:15 +0530

    Update and rename README.md to ConditionalStatements.js

---
+++ Javascript/6_Conditional_Statements/ConditionalStatements.js
@@ -0,0 +1,14 @@
+let marks = 85;
+
+if (marks >= 90) {
+  console.log("Grade: A");
+} else if (marks >= 75) {
+  console.log("Grade: B");
+} else {
+  console.log("Grade: C");
+}
+
+// Ternary operator
+let result = marks >= 40 ? "Pass" : "Fail";
+console.log(result);
+
