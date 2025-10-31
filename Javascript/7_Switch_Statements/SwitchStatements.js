fae00595483e4b964e3a72c28f93962abf382410
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-10-31 20:19:47 +0530

    Update and rename README.md to SwitchStatements.js

---
+++ Javascript/7_Switch_Statements/SwitchStatements.js
@@ -0,0 +1,12 @@
+let day = "Tuesday";
+
+switch (day) {
+  case "Monday":
+    console.log("Start of the week!");
+    break;
+  case "Tuesday":
+    console.log("Second day!");
+    break;
+  default:
+    console.log("Another day!");
+}
