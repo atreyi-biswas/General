5b95e171b8f1ab61fc5a8acb999df24e5efe0825
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-11-03 01:56:44 +0530

    Update loops.js

--- Javascript/8_Loops/loops.js
+++ Javascript/8_Loops/loops.js
@@ -1,1 +1,15 @@
+for (let i = 1; i <= 5; i++) {
+  console.log("For Loop:", i);
+}
 
+let j = 1;
+while (j <= 3) {
+  console.log("While Loop:", j);
+  j++;
+}
+
+let k = 1;
+do {
+  console.log("Do-While Loop:", k);
+  k++;
+} while (k <= 2);
