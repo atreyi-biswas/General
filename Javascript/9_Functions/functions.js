0332fc744bf2371e03d781580bbb3b899774c4fe
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-11-03 01:57:27 +0530

    Update functions.js

--- Javascript/9_Functions/functions.js
+++ Javascript/9_Functions/functions.js
@@ -1,1 +1,13 @@
+function greet(name) {
+  return "Hello, " + name;
+}
 
+const add = function(a, b) {
+  return a + b;
+};
+
+const multiply = (x, y) => x * y;
+
+console.log(greet("Atreyi"));
+console.log(add(2, 3));
+console.log(multiply(3, 4));
