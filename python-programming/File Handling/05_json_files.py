e1a573150e70f93488f2ee3e165323cc554093be
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-11-09 12:54:22 +0530

    Update 05_json_files.py

--- python-programming/File Handling/05_json_files.py
+++ python-programming/File Handling/05_json_files.py
@@ -1,1 +1,10 @@
 
+import json
+data = {"name":"Atreyi","skills":["python","c"]}
+with open("data.json","w") as f:
+    json.dump(data, f, indent=2)
+
+with open("data.json") as f:
+    loaded = json.load(f)
+print(loaded)
+
