0e9f4a443be2b75e8b546d9fca29d1b97e1d19fe
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-11-10 22:32:25 +0530

    Update 06_class_vs_static.py

--- python-programming/OOP/06_class_vs_static.py
+++ python-programming/OOP/06_class_vs_static.py
@@ -1,1 +1,10 @@
+class MyClass:
+    count = 0
+    def __init__(self): MyClass.count += 1
+    @classmethod
+    def get_count(cls): return cls.count
+    @staticmethod
+    def info(): return "utility"
 
+a = MyClass(); b = MyClass()
+print(MyClass.get_count(), MyClass.info())
