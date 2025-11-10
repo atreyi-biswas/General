3c67c708e00b0e22c1973801bdec4ca876019d65
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-11-10 22:31:56 +0530

    Update 04_encapsulation.py

--- python-programming/OOP/04_encapsulation.py
+++ python-programming/OOP/04_encapsulation.py
@@ -1,1 +1,11 @@
+class BankAccount:
+    def __init__(self, bal=0):
+        self._balance = bal
+    def deposit(self, amt):
+        if amt>0: self._balance += amt
+    def balance(self):
+        return self._balance
 
+acct = BankAccount(100)
+acct.deposit(50)
+print(acct.balance())
