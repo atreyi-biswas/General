32a366730ab983ff9fb64afbea134e2f2204b6f7
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-11-10 22:32:10 +0530

    Update 05_magic_methods.py

--- python-programming/OOP/05_magic_methods.py
+++ python-programming/OOP/05_magic_methods.py
@@ -1,1 +1,7 @@
+class Word:
+    def __init__(self, text): self.text = text
+    def __str__(self): return self.text
+    def __len__(self): return len(self.text)
 
+w = Word("python")
+print(str(w), len(w))
