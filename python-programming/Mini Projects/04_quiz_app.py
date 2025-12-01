a88b395c76a92cdffc634be7569fa181a0e2f502
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-12-01 15:40:48 +0530

    Update 04_quiz_app.py

--- python-programming/Mini Projects/04_quiz_app.py
+++ python-programming/Mini Projects/04_quiz_app.py
@@ -1,1 +1,15 @@
+questions = {
+    "Capital of India? ": "Delhi",
+    "5 + 7 = ? ": "12",
+    "Python creator? ": "Guido"
+}
+
+score = 0
+
+for q, a in questions.items():
+    ans = input(q)
+    if ans.lower() == a.lower():
+        score += 1
+
+print("Score:", score, "/", len(questions))
 
