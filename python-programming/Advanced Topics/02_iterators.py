46814aa81a93d263f1fb551855f5cac7a3b34d0b
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-12-01 15:36:08 +0530

    Update 02_iterators.py

--- python-programming/Advanced Topics/02_iterators.py
+++ python-programming/Advanced Topics/02_iterators.py
@@ -1,1 +1,18 @@
+class MyIterator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.num = 0
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.num < self.limit:
+            self.num += 1
+            return self.num
+        else:
+            raise StopIteration
+
+it = MyIterator(5)
+for val in it:
+    print(val)
