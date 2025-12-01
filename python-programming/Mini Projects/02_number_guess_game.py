ea09f29a4f13dfefc82c58c16544beba8f9c132f
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-12-01 15:39:54 +0530

    Update 02_number_guess_game.py

--- python-programming/Mini Projects/02_number_guess_game.py
+++ python-programming/Mini Projects/02_number_guess_game.py
@@ -1,1 +1,10 @@
+import random
 
+num = random.randint(1, 20)
+guess = 0
+
+while guess != num:
+    guess = int(input("Guess number 1–20: "))
+    if guess < num: print("Too low!")
+    elif guess > num: print("Too high!")
+    else: print("Correct!")
