0216f2c1fdc7840d4f52002a89183bb025c69955
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-12-01 15:42:11 +0530

    Update 05_simple_chatbot.py

--- python-programming/Mini Projects/05_simple_chatbot.py
+++ python-programming/Mini Projects/05_simple_chatbot.py
@@ -1,1 +1,10 @@
+while True:
+    msg = input("You: ")
 
+    if "hi" in msg.lower():
+        print("Bot: Hello!")
+    elif "bye" in msg.lower():
+        print("Bot: Bye!")
+        break
+    else:
+        print("Bot: I don’t understand.")
