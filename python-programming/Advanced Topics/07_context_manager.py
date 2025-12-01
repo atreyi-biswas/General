aace208ce520075b301efd8b1227d3ab92b9dd8d
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-12-01 15:38:34 +0530

    Update 07_context_manager.py

--- python-programming/Advanced Topics/07_context_manager.py
+++ python-programming/Advanced Topics/07_context_manager.py
@@ -1,1 +1,14 @@
+class FileOpen:
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
 
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc, traceback):
+        self.file.close()
+
+with FileOpen("sample.txt", "w") as f:
+    f.write("Hello World")
