1c84ea6bdabfe4e5023fc22b7af47a13258fe02e
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-12-01 15:40:36 +0530

    Update 03_todo_cli.py

--- python-programming/Mini Projects/03_todo_cli.py
+++ python-programming/Mini Projects/03_todo_cli.py
@@ -1,1 +1,19 @@
+todos = []
 
+while True:
+    print("\n1.Add  2.View  3.Remove  4.Exit")
+    choice = input("Choose: ")
+
+    if choice == '1':
+        todos.append(input("Enter task: "))
+
+    elif choice == '2':
+        for i, t in enumerate(todos):
+            print(i, t)
+
+    elif choice == '3':
+        idx = int(input("Index to remove: "))
+        todos.pop(idx)
+
+    elif choice == '4':
+        break
