8f5a0bac3eff17708bcf0dc4b9a528b338fe3575
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-12-01 15:36:27 +0530

    Update 03_decorators.py

--- python-programming/Advanced Topics/03_decorators.py
+++ python-programming/Advanced Topics/03_decorators.py
@@ -1,1 +1,12 @@
+def logger(func):
+    def wrapper():
+        print("Function started")
+        func()
+        print("Function ended")
+    return wrapper
 
+@logger
+def greet():
+    print("Hello!")
+
+greet()
