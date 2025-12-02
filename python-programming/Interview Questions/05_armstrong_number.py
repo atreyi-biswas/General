fb6d5f411e91f2268bd471d0bf0592b7391b439d
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-12-02 12:37:06 +0400

    Update 05_armstrong_number.py

--- python-programming/Interview Questions/05_armstrong_number.py
+++ python-programming/Interview Questions/05_armstrong_number.py
@@ -1,1 +1,8 @@
+n = input("Enter number: ")
+digits = list(map(int, n))
+power = len(digits)
+
+total = sum(d ** power for d in digits)
+
+print("Armstrong" if total == int(n) else "Not Armstrong")
 
