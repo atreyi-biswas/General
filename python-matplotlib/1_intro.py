5f2f74bc9480bdfa13ed8ba70149172fa2ddcfa8
Atreyi Biswas <atreyi.biswas@outlook.com>
2025-12-07 13:41:27 +0400

    Update and rename intro.py to 1_intro.py

---
+++ python-matplotlib/1_intro.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+x = [2023, 2024, 2025, 2026]
+y = [15, 25, 30, 20]
+
+plt.plot(x, y)
+
+plt.show()
+
+# use numpy to run the program faster:
+# import matplotlib.pyplot as plt
+# import numpy as np
+# x = np.array([2023, 2024, 2025, 2026])
+# y = np. array([15, 25, 30, 20])
+# plt.plot(x, y)
+# plt.show()
