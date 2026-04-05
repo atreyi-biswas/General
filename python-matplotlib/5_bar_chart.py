6b279b1ab04edc16721c88d1e6268843eaa70e07
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-04-05 20:28:45 +0530

    Update 5_bar_chart.py

--- python-matplotlib/5_bar_chart.py
+++ python-matplotlib/5_bar_chart.py
@@ -4,5 +4,9 @@ import numpy as np
 x = np.array(['Fruits', 'Vegetables', 'Chicken'])
 y = np.array([5, 10, 15])
 
-plt.bar(x,y)
+plt.title("Bar Graph")
+plt.xlabel("Food")
+plt.ylabel("Quantity")
+
+plt.bar(x,y, color=['red', 'green', 'blue']) #plt.barh(x,y) for horizontal bar graph
 plt.show()
