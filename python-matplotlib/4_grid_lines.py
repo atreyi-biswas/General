6fc4b35f3de79945258e6b0e80f6e5c6839df250
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-04-05 20:14:45 +0530

    Update 4_grid_lines.py

--- python-matplotlib/4_grid_lines.py
+++ python-matplotlib/4_grid_lines.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.grid()
+plt.grid( axis = 'y',   
+        color = 'black', 
+        linewidth = "1", 
+        linestyle='dashed') # dotted, dot-dashed, solid
 
 x = [2, 4, 6, 8, 10]
 y = [5, 10, 15, 20, 25]
