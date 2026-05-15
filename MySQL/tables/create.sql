640245ad4dfe32410af467365c4eab78023f8678
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-05-15 11:51:51 +0530

    Update create.sql

--- MySQL/tables/create.sql
+++ MySQL/tables/create.sql
@@ -12,13 +12,18 @@ CREATE TABLE employees (
 
 -- To drop(remove) table : DROP employees;
 
--- To alter table : ALTER TABLE employees;
-;--                  ADD phone_number VARCHAR(15);                    
+-- To alter table : ALTER TABLE employees
+--                  ADD phone_number VARCHAR(15);                    
 
 -- * : All
 
--- To rename table : ALTER TABLE employees;
+-- To rename table : ALTER TABLE employees
 --                   RENAME COLUMN phone_number TO email;   {but the email data type is the same as phone_number datatype, i.e, VARCHAR}
 
--- To modify : ALTER TABLE employees;
+-- To modify : ALTER TABLE employees
 --             MODIFY COLUMN email VARCHAR(100)
+
+-- To move columns in a table : ALTER TABLE employees
+--                              MODIFY email VARCHAR(100)
+--                              AFTER last_name;               {or if you want to bring it to first column, then write, FIRST;}
+--                              SELECT * FROM employees;
