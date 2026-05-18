6e0c05f75f58593268f39af82c2dd7748191deb8
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-05-18 12:36:30 +0530

    Update updateDelete.sql

--- MySQL/updateDelete.sql
+++ MySQL/updateDelete.sql
@@ -3,3 +3,15 @@ UPDATE employees
 SET hourly_pay = 5.25
 WHERE employee_id = 1;
 SELECT * FROM employees;
+
+--To UPDATE multiple columns:
+UPDATE employees
+SET hourly_pay = 5.25,
+  first_name = "xyz"
+WHERE employee_id = 1;
+SELECT * FROM employees;
+
+--To UPDATE consistent values accross the column:
+UPDATE employees
+SET hourly_pay = 5.25,
+SELECT * FROM employees;
