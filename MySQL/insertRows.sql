ad87da53bbce35a1d1417ab02c9e48809b5d4bc6
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-05-18 09:13:10 +0530

    Update insertRows.sql

--- MySQL/insertRows.sql
+++ MySQL/insertRows.sql
@@ -4,6 +4,6 @@ VALUES (1, "Mairnett", "DuPengChen", 25.00, "2026-05-17"),
 	(3, "Chloe", "Bougua", 30.00, "2026-05-19"),
 	(4, "Gabriel", "Agrestte", 10.00, "2026-05-18");
 
-INSERT INTO employees(employee_id, first_name, last_name)
-VALUES (4, "Jaged", "Stone");
+-- INSERT INTO employees(employee_id, first_name, last_name)     {to insert some details of employee}
+-- VALUES (4, "Jaged", "Stone");
 SELECT * FROM employees;
