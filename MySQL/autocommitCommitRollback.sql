1050149acfc0dcac91b6030c8f29d013a5e332e8
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-05-20 11:56:35 +0530

    Update autocommitCommitRollback.sql

--- MySQL/autocommitCommitRollback.sql
+++ MySQL/autocommitCommitRollback.sql
@@ -1,1 +1,16 @@
+SET AUTOCOMMIT = OFF;
+COMMIT;  --create a safe point
 
+DELETE FROM employees;
+SELECT * FROM employees; -- Table deleted
+
+--can undo all these changes because of commit by the help of ROLLBACK statement
+
+ROLLBACK;
+SELECT * FROM employees; --Table back to how it previously was
+
+--to save changes the following changes...
+DELETE FROM employees;
+SELECT * FROM employees; 
+-- have to commit after changes
+COMMIT;
