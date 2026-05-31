587dedd95249a5c1ba2de3e29f9c02d620f82213
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-05-31 18:43:21 +0400

    Update 8_histograms.py

--- python-matplotlib/8_histograms.py
+++ python-matplotlib/8_histograms.py
@@ -1,1 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+scores = np.random.normal(loc=80, scale=10, size=100)
+scores = np.clip(scores,0,100)
+
+plt.hist(scores, bins=10,
+        color='blue', 
+        edgecolor='black')
+
+plt.title('Exam Scores')
+plt.xlabel('Score')
+plt.ylabel('No. of Students')
+
+plt.show()
