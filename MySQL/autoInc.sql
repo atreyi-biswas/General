ce59fc50871813a8b9dd22a0cedfa7f9e227c982
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-03 00:04:45 +0400

    Update autoInc.sql

--- MySQL/autoInc.sql
+++ MySQL/autoInc.sql
@@ -1,1 +1,10 @@
+-- Auto increment automatically increases the transaction_id
+CREATE TABLE transactions (
+  transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+  amount DECIMAL(5, 2) 
+);
 
+-- Hence writing only values of amount is enough, the transaction_id automatically sequences itself
+INSERT INTO transactions(amount)
+VALUES(5.67);
+SELECT * FROM transactions;
