c125b4350b1a7b63d6f8e7dc1216b6e905d0d272
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-02 16:31:41 +0400

    Update cdCt.sql

--- MySQL/cdCt.sql
+++ MySQL/cdCt.sql
@@ -1,1 +1,16 @@
+INSERT TABLE test(
+  my_date DATE,
+  my_time TIME,
+  my_datetime DATETIME
+);
 
+
+
+INSERT INTO test
+VALUES(CURRENT_DATE() , CURRENT_TIME(), NOW());
+SELECT * FROM test;
+
+
+INSERT INTO test
+VALUES(CURRENT_DATE() +1 , NULL, NULL);
+SELECT * FROM test;
