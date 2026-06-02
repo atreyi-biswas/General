8677dc7f2faee565a3ec13ff9142c89ec08bcfbc
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-02 16:51:41 +0400

    Update notNull.sql

--- MySQL/notNull.sql
+++ MySQL/notNull.sql
@@ -1,1 +1,9 @@
+ALTER TABLE products
+MODIFY price DECIMAL(4, 2) NOT NULL;
 
+
+-- The following query will not be executed because price should be NOT NULL
+INSERT INTO products
+VALUES (104, "cookie", NULL);
+
+SELECT * FROM products;
