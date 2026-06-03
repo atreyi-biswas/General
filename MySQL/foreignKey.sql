612513881a08213d34bd77e07b7d20bab44a0085
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-03 14:16:54 +0400

    Update foreignKey.sql

--- MySQL/foreignKey.sql
+++ MySQL/foreignKey.sql
@@ -1,3 +1,6 @@
+-- FOREIGN KEY : Primary key from one table that's found in another table
+
+
 CREATE TABLE transactions(
 		transaction_id INT PRIMARY KEY AUTO_INCREMENT,
     amount DECIMAL(5, 2),
@@ -5,9 +8,30 @@ CREATE TABLE transactions(
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
--- i created another table of "customers", where there's a similar column called "customer_id"
-CREATE TABLE customers(  
+-- i created another table of "customers", where there's a similar column called "customer_id", which MUST be declared as a primary key, for it to be a foreign key.
+CREATE TABLE customers( 
     customer_id INT PRIMARY KEY AUTO_INCREMENT,     
     first_name VARCHAR(50),     
 		last_name VARCHAR(50) 
 	)
+
+-- To drop foreign key from a table
+ALTER TABLE transactions
+DROP FOREIGN KEY transactions_ibfk_1
+-- transactions_ibfk_1 is the name of the foreign key in the schemas
+
+-- to add foreign key after table is created
+ALTER TABLE transactions
+ADD CONSTRAINT fk_customer_id  -- we do not necessarily need to name the foreign key constraint
+FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+
+-- now we have a link between customers table and transactions table
+ALTER TABLE transactions 
+AUTO_INCREMENT = 1000
+INSERT INTO transactions(amount, customer_id)
+VALUES (4.99, 3),
+	(2.34, 2),
+    (3.34, 3),
+    (4.67, 1);
+
+SELECT * FROM transactions;
