d9abee297f2debb059c4696132041841cd2fbc10
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-03 13:30:06 +0400

    Update foreignKey.sql

--- MySQL/foreignKey.sql
+++ MySQL/foreignKey.sql
@@ -1,1 +1,13 @@
+CREATE TABLE transactions(
+		transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+    amount DECIMAL(5, 2),
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
 
+-- i created another table of "customers", where there's a similar column called "customer_id"
+CREATE TABLE customers(  
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,     
+    first_name VARCHAR(50),     
+		last_name VARCHAR(50) 
+	)
