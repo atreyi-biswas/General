8e1effd9a58e014b23c25188cc29240535743648
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-04 19:33:38 +0400

    Update joins.sql

--- MySQL/joins.sql
+++ MySQL/joins.sql
@@ -3,8 +3,13 @@
 SELECT * 
 FROM transactions INNER JOIN customers	
 ON transactions.customer_id = customers.customer_id;
-
 /* 
+
+transaction_id | amount	| customer_id	| customer_id	| first_name | last_name
+1003	         | 4.67	  | 1	          | 1	          | Fred	     | Fish
+1001	         | 2.34	  | 2	          | 2	          | Atreyi	   | Chicken
+1000	         | 4.99	  | 3	          | 3	          | Biswas	   | Veggies
+1002           | 3.34	  | 3	          | 3	          | Biswas	   | Veggies
 
 */
 
@@ -13,6 +18,7 @@ SELECT transaction_id, amount, first_name, last_name
 FROM transactions INNER JOIN customers	
 ON transactions.customer_id = customers.customer_id;
 /* 
+
 transaction_id  |  amount | First_name | last_name
 1000	          | 4.99	  | Biswas	   | Veggies
 1001	          | 2.34	  | Atreyi	   | Chicken
