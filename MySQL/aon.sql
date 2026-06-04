9a93a32c19a3cb8ae05bc2f02df4662e2f7ffab3
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-04 22:48:57 +0400

    Update aon.sql

--- MySQL/aon.sql
+++ MySQL/aon.sql
@@ -1,1 +1,15 @@
+-- using AND : Both statements should be true
+SELECT *
+FROM employees
+WHERE hire_date > "2023-01-05"  AND job = cook;
+
+-- using OR : Either statements should be true
+SELECT *
+FROM employees
+WHERE job = cook OR job = cashier;
+
+-- using NOT : Statement should be false
+SELECT *
+FROM employees
+WHERE NOT job = manager;
 
