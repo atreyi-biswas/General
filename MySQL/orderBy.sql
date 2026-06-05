a357bcce4383d233454e86aa33f8f47f1ce8ddfe
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-05 13:29:26 +0400

    Update orderBy.sql

--- MySQL/orderBy.sql
+++ MySQL/orderBy.sql
@@ -1,1 +1,15 @@
+-- Order by Descending order:
+SELECT * FROM employees
+ORDER BY last_name DESC;
 
+-- Order by Ascending order:   {default}
+SELECT * FROM employees
+ORDER BY last_name ASC;
+
+-- If multiple values are same, we can include some other unique column, example:
+SELECT * FROM transactions
+ORDER BY amount, customer_id ;  -- {By default ascending}
+
+--or we can do this too :
+SELECT * FROM transactions
+ORDER BY amount ASC, customer_id DESC;
