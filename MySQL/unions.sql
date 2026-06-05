87bcd8cc878b63c6138f76811be7be24b184d4c1
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-05 16:39:33 +0400

    Update unions.sql

--- MySQL/unions.sql
+++ MySQL/unions.sql
@@ -1,1 +1,11 @@
+-- Unions : Combines two or more Select Statemnts
+SELECT * FROM income
+UNION 
+SELECT * FROM expenses;
 
+-- Only works if both the tables have same number of columns, so we can select what columns we want to include, example:
+SELECT first_name, last_name FROM employees
+UNION 
+SELECT first_name, last_name FROM customers;
+
+UNION ALL -- : Includes all duplicates
