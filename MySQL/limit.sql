02dad274865284f1ae884fcdc062d905b8870702
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-05 13:37:30 +0400

    Update limit.sql

--- MySQL/limit.sql
+++ MySQL/limit.sql
@@ -1,1 +1,14 @@
+-- Limit : Limit number of records
+-- used to display large data on pages (pagination)
 
+SELECT * FROM employees
+LIMIT 5; -- Default limits by customer_id
+
+SELECT * FROM employees
+ORDER BY last_name DESC LIMIT 5;
+
+--Display some number of records after an offset :
+SELECT * FROM employees
+LIMIT 10, 1;  -- skip 10 rows, return 1 row after that
+
+-- offset = how many rows to skip  ,  count = how many rows to return
