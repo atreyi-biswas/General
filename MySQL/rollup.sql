04030d0907d395cc57cf02f30d4237ed3915ca81
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-09 14:48:23 +0400

    Update rollup.sql

--- MySQL/rollup.sql
+++ MySQL/rollup.sql
@@ -1,1 +1,8 @@
+-- ROLLUP clause
+-- extention of the GROUP BY clause
+-- produces another row and shows the grand total(super aggregiate value)
 
+SELECT SUM(amount), order_date
+FROM transactions
+GROUP BY order_date
+WITH ROLLUP;
